08c729ce95e497031edb3393d151c96914d73a82
GiulioMastromartino <151667126+GiulioMastromartino@users.noreply.github.com>
2026-02-05 05:42:22 +0100

    Miglioramento gs.m: output residuo, correzione splitting M-N

--- gs.m
+++ gs.m
@@ -1,6 +1,6 @@
-function [x,k] = gs(A,b,x0,toll,nmax)
+function [x,k,res_norm] = gs(A,b,x0,toll,nmax)
 %
-% [x,k] = gs(A,b,x0,toll,nmax)
+% [x,k,res_norm] = gs(A,b,x0,toll,nmax)
 %
 % Metodo di Gauss-Seidel per l'approssimazione della soluzione di A x = b
 %
@@ -14,13 +14,14 @@ function [x,k] = gs(A,b,x0,toll,nmax)
 % Parametri di uscita:
 % x        Approssimazione del vettore soluzione, vettore colonna
 % k        Numero di iterazioni effettuate
+% res_norm Residuo normalizzato finale ||b - Ax_k|| / ||b||
 %
 %                                         Politecnico di Milano, 04/04/2024
 %
 
 n = length(b);
-xn = zeros( n, 1 );
-k = 0;
+b = b(:);
+x0 = x0(:);
 
 if (( size(A,1)~=n) || (size(A,2)~=n) || (length(x0) ~= n) )
   error('dimensioni incompatibili')
@@ -30,19 +31,35 @@ if (prod(diag(A)) == 0)
     error('errore: elementi diagonali nulli')
 end
 
-T = tril(A);
+% Matrice triangolare inferiore (parte di M per lo splitting)
+M = tril(A); 
+% Parte strettamente superiore (N nello splitting A = M - N)
+N = -triu(A, 1);
+
 xv = x0;
 r = b - A * x0;
-err = norm(r) / norm(b);
+norm_b = norm(b);
+if norm_b == 0, norm_b = 1; end % Evita div by zero
+
+err = norm(r) / norm_b;
+k = 0;
 
 while ( err > toll && k < nmax )
   k = k + 1;
-  z = fwsub(T,r);
-  xn = xv + z;
-  r = b - A*xn;  
-  err = norm(r) / norm(b);
+  
+  % Risolvo M * x_{k+1} = N * x_k + b
+  % x_{k+1} = M \ (N * x_k + b)
+  % Poiché M è tril, usiamo fwsub
+  rhs = N * xv + b;
+  xn = fwsub(M, rhs);
+  
+  r = b - A * xn;
+  err = norm(r) / norm_b;
   xv = xn;
 end
 
-x = xn;
+x = xv;
+res_norm = err;
 
+return
+end
