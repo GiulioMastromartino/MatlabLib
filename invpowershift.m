49043a5bcc9ec2bc22a1461d4d4915085ec111df
GiulioMastromartino <151667126+GiulioMastromartino@users.noreply.github.com>
2026-02-05 05:42:12 +0100

    Miglioramento invpowershift.m: gestione nargin, errore relativo, Rayleigh corretto

--- invpowershift.m
+++ invpowershift.m
@@ -2,7 +2,7 @@ function [lambda,x,iter] = invpowershift(A,mu,tol,nmax,x0)
 % 
 % [lambda,x,iter] = invpowershift(A,mu,tol,nmax,x0)
 %
-% Metodo delle potenze inversecon shift per approssimare l'autovalore di 
+% Metodo delle potenze inverse con shift per approssimare l'autovalore di 
 % di una matrice A (A x = lambda x) piu' prossimo allo shift mu. Il sistema
 % lineare viene risolto applicando il metodo della fattorizzazione LU.
 %
@@ -26,34 +26,70 @@ function [lambda,x,iter] = invpowershift(A,mu,tol,nmax,x0)
 
 [n,m] = size(A);
 if n ~= m, error('Solo per matrici quadrate'); end
-if nargin == 2
-   tol = 1.e-06;   x0 = ones(n,1);   nmax = 100;
-end
-M = A - mu*eye(n);
-% calcolo la fattorizzazione LU una volta per tutte
-[L,U,P]=lu(M);
-% iterazione zero fuori dal ciclo while
-iter = 0;
-y = x0/norm(x0); % y0
-lambda = y'*A*y; % lambda0
-err = tol*abs(lambda) + 1; % dobbiamo entrare nel ciclo
 
-while (err>tol*abs(lambda)) && (abs(lambda)~=0) && (iter<nmax)
-   iter = iter + 1; % iter=1,2,3,...
-   % risolvo Mx^{(k)}=y^{(k-1)}
-   z=fwsub(L,P*y);
-   x=bksub(U,z);
-   y= x/norm(x);
-   lambdanew = y'*A*y;
-   err = abs(lambdanew - lambda);
+% Gestione parametri opzionali
+if nargin < 5
+    x0 = ones(n,1);
+end
+if nargin < 4
+    nmax = 100;
+end
+if nargin < 3
+    tol = 1.e-06;
+end
+
+% Matrice shiftata M = A - mu*I
+M = A - mu*eye(n);
+
+% Fattorizzazione LU una volta per tutte
+[L,U,P] = lu(M);
+
+% Inizializzazione
+iter = 0;
+x0 = x0(:); % Assicura colonna
+if norm(x0) == 0, error('Vettore iniziale nullo'); end
+
+y = x0 / norm(x0); 
+% Stima iniziale dell'autovalore (quoziente di Rayleigh)
+lambda = y' * A * y; 
+
+% Entra nel ciclo almeno una volta
+err = tol * abs(lambda) + 1; 
+
+while (err > tol * abs(lambda)) && (iter < nmax)
+   iter = iter + 1;
+   
+   % Risolvo (A - mu*I) * x_k = y_{k-1}
+   % Usando la fattorizzazione: L*U*x = P*y
+   z = fwsub(L, P*y);
+   x_k = bksub(U, z);
+   
+   % Normalizzo
+   y = x_k / norm(x_k);
+   
+   % Quoziente di Rayleigh per lambda
+   lambdanew = y' * A * y;
+   
+   % Errore relativo sull'autovalore
+   if abs(lambda) > eps
+       err = abs(lambdanew - lambda) / abs(lambda);
+   else
+       err = abs(lambdanew - lambda);
+   end
+   
    lambda = lambdanew; 
 end
+
+% Assegna autovettore finale
+x = y;
+
 if (iter < nmax)
      fprintf(['Il metodo delle potenze inverse con shift converge ',...
-              'in %d iterazioni all''autovalore \n'], iter);
-     lambda
+              'in %d iterazioni all''autovalore \\n'], iter);
 else
      fprintf(['Il metodo delle potenze inverse con shift non converge ',...
-              'in %d iterazioni. \n'], iter);
+              'in %d iterazioni. \\n'], iter);
 end
+
 return
+end
